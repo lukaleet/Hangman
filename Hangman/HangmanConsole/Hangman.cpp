c632734c1d58a4c7ab278405c3f0cffe0b716304
lukaleet <lukamar495@student.polsl.pl>
2019-12-30 17:11:40 +0100

    Added Function To Pick Random Word, And Game Setup Function. Added Support To Uppercase Characters.
    Added working implementation of pickRandomWord. Function returns random word at index [0] of given vector after a shuffle of the vector.
    
    Uppercase words and input characters are supported now.

--- Hangman/HangmanConsole/Hangman.cpp
+++ Hangman/HangmanConsole/Hangman.cpp
@@ -8,24 +8,88 @@
 
 using namespace std;
 
+void game(string);
 vector<string> readWordsFromFile(string);
-string pickRandomWord(vector<string> words);
+string pickRandomWord(vector<string>);
 
 int main()
 {
-	vector<string> words = readWordsFromFile("slowa.txt");
-	string word = pickRandomWord(words);
-
-	for (int i = 0; i < words.size(); i++) 
+	char input;
+	while (true) 
 	{
-		cout << "index = " << i << " " << words[i] << endl;
+		cout << "Chcesz zagrac? y/n" << endl;
+		cin >> input;
+		input = tolower(input);
+		if (input == 'y')
+		{
+			vector<string> words = readWordsFromFile("slowa.txt");
+			string word = pickRandomWord(words);
+
+			//testowe printowanie
+			for (int i = 0; i < words.size(); i++)
+			{
+				cout << "index = " << i << " " << words[i] << endl;
+			}
+
+			cout << word << endl;
+			game(word);
+		}
+		else if (input == 'n') 
+		{
+			break;
+		}
+		else 
+		{
+			cout << "Nie zrozumialem :(" << endl;
+		}
 	}
-
-	//cout << words.size() << endl;
-
-	//cout << word;
 }
 
+void game(string randomWord) 
+{
+	string secretWord;
+	int guessCounter = randomWord.size();
+
+	cout << "Wylosowane slowo ma " << randomWord.size() << " liter" << endl;
+
+	for (int i = 0; i < randomWord.size(); i++) 
+	{
+		secretWord.push_back('*');
+	}
+
+	while(true) 
+	{
+		if(secretWord == randomWord) 
+		{
+			cout << "Wylosowanym slowem bylo: " << randomWord << "." << " Wygrales!" << endl;
+			break;
+		}
+
+		else if (guessCounter == 0) 
+		{
+			cout << "Wylosowanym slowem bylo: " << randomWord << "." << " Przegrales!" << endl;
+			break;
+		}
+
+		cout << "Zostalo Ci " << guessCounter << " prob" << endl;
+		char playerInput;
+		cin >> playerInput;
+		playerInput = tolower(playerInput);
+		if (playerInput == 'q')
+		{
+			break;
+		}
+		for (int i = 0; i < randomWord.size(); i++) 
+		{
+			if (randomWord[i] == playerInput) 
+			{
+				secretWord[i] = playerInput;
+			}
+		}
+		cout << secretWord << endl;
+		guessCounter -= 1;
+	}
+}
 
 // funkcja wczytujaca wyrazy z pliku linijka po linijce slowo po slowie; zwraca liste slow
 vector<string> readWordsFromFile(string path) 
@@ -34,17 +98,18 @@ vector<string> readWordsFromFile(string path)
 	string word, line;
 	vector<string> wordsList;
 
-	if (!fileStream.is_open()) 
+	if(!fileStream.is_open()) 
 	{
 		cout << "Nie udalo otworzyc sie pliku." << endl;
 	}
 
-	while (fileStream >> ws, getline(fileStream, line))
+	while(fileStream >> ws, getline(fileStream, line))
 	{
 		stringstream ss(line);
 
-		while (getline(ss, word, ','))
+		while(getline(ss, word, ','))
 		{
+			transform(word.begin(), word.end(), word.begin(), ::tolower);
 			wordsList.push_back(word);
 		}
 	}
@@ -53,16 +118,15 @@ vector<string> readWordsFromFile(string path)
 	return wordsList;
 }
 
-// funkcja wybierajaca losowe slowo z listy slow; jeszcze nie dziala
+// funkcja wybierajaca losowe slowo z listy slow
 string pickRandomWord(vector<string> words) 
 {
-	int size = words.size();
-	string randomWord;
+	random_device randomDevice;
 
-	auto rng = default_random_engine{};
+	auto rng = default_random_engine{randomDevice()};
 	shuffle(begin(words), end(words), rng);
 
-	randomWord = words[0];
+	string randomWord = words[0];
 
 	return randomWord;
 }
